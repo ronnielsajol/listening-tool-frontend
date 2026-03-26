3e61c6a86c344c2ce2bc8f0887e067f3d499eddf
Ronniel Sajol <sajolronniel28@gmail.com>
2026-03-26 16:33:24 +0800

    fixed fetching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	serverExternalPackages: ["apify-client"],
+	serverExternalPackages: ["apify-client", "proxy-agent"],
 };
 
 export default nextConfig;
